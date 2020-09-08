0b15073bbadc9971aea4aeab7f76a838f59c52c3
itsmohitj <itsmohitj@gmail.com>
2020-09-08 11:14:17 +0530

    Added Problem 1108: Defanging an IP Address

---
+++ defanIPaddr.py
@@ -0,0 +1,10 @@
+#Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+#A defanged IP address replaces every period "." with "[.]".
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".","[.]")
+
+# Time Complexity : O(1)
+# Space Complexity : O(1)
