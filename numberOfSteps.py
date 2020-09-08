af49ff71b62746236fc35756216a57d4b2fd6a5c
itsmohitj <itsmohitj@gmail.com>
2020-09-08 11:41:13 +0530

    Addd Problem 1342

---
+++ numberOfSteps.py
@@ -0,0 +1,15 @@
+#Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        count = 0
+        while(num!=0):
+            if num % 2 == 0:
+                num = num // 2
+            else:
+                num = num - 1
+            count+=1
+        return count
+
+# Time Complexity = O(n)
+# Space Complexity = O(1)
