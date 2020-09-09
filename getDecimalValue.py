5b7dd2ff8d9b5320df7ced8def22e4790de3c970
itsmohitj <itsmohitj@gmail.com>
2020-09-09 13:36:24 +0530

    Added Problem 1290

---
+++ getDecimalValue.py
@@ -0,0 +1,19 @@
+# Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+# Return the decimal value of the number in the linked list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        ans = 0
+        while head:
+            ans = 2*ans + head.val
+            head = head.next
+        return ans
+
+# Time Complexity = O(n) where n is the number of nodes in the linked list.
+# Space Complexity = O(1) as No extra space is used
