d2285d79538cf53f100600dc7bd618824fe2393e
itsmohitj <itsmohitj@gmail.com>
2020-09-09 19:33:52 +0530

    Added Problem 709

---
+++ toLowerCase.py
@@ -0,0 +1,8 @@
+#Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+
+# Time Complexity = O(n)
+# Space Complexity = O(1)
