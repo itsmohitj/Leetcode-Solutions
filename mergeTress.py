7f7852085c052f4bad60110e14dff0986583910a
itsmohitj <itsmohitj@gmail.com>
2020-09-11 13:07:39 +0530

    Added Problem 617

---
+++ mergeTress.py
@@ -0,0 +1,43 @@
+"""
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Input:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+Output:
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+
+# Time complexity : O(m). A total of n nodes need to be traversed. Here, n represents the minimum number of nodes from the two given trees.
+
+# Space complexity : O(n). The depth of the recursion tree can go upto n in the case of a skewed tree. In average case, depth will be O(logn).
+
+
