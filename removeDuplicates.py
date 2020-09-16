b13deb18daf2eae3126ba0b855b75a48406f8f75
itsmohitj <itsmohitj@gmail.com>
2020-09-16 15:49:31 +0530

    Added Problem 1047

---
+++ removeDuplicates.py
@@ -0,0 +1,26 @@
+"""
+Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+
+We repeatedly make duplicate removals on S until we no longer can.
+
+Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
+
+Input: "abbaca"
+Output: "ca"
+Explanation:
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+"""
+
+
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stack = []
+        for c in S:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        return "".join(stack)
+
+# Time Complexity = O(n) where n is the length of the string
+# Space Complexity = O(n) where n is the length of the stack
